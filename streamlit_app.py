c6e58cd597a7579aefbf24d8f945a1850554ada6
Dev <79055244+dev-founder-titan@users.noreply.github.com>
2023-05-30 22:56:52 -0400

    Update streamlit_app.py
    Made minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("My parents' new healthy diner!")
 
-streamlit.header("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.header("Breakfast favourites")
+
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado toast")
