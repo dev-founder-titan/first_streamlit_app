b25b228c6050c5edc0fda8ced47b2a417d17348b
Dev <79055244+dev-founder-titan@users.noreply.github.com>
2023-05-30 22:51:28 -0400

    Update streamlit_app.py
    Added emoji's to the menu with a new subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("My parents' new healthy diner!")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 with Oatmeal")
-streamlit.text("Veggie Omlette")
-streamlit.text("Toast with apple juice")
+streamlit.header("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
